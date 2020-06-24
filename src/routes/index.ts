a660036c6a4476f4c2178abb9c715ac1de28e312
Gabriel <gprando55@gmail.com>
2020-06-24 19:47:49 -0300

    create token

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import appointmentsRouter from './_appointments.routes';
 import usersRouter from './_users.routes';
+import sessionsRouter from './_sessions.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
 routes.use('/users', usersRouter);
+routes.use('/sessions', sessionsRouter);
 
 export default routes;
