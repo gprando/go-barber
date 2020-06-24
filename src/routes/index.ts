2b347e2af229e595a71b0922a4627b35b2c2930a
Gabriel <gprando55@gmail.com>
2020-06-24 18:05:00 -0300

    user

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
-import appointmentsRouter from './appointments.routes';
+import appointmentsRouter from './_appointments.routes';
+import usersRouter from './_users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
 
 export default routes;
