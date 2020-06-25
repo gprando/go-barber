32e45d3f5d588ef6dc1aed7634a3ecfcb8af3d8f
Gabriel <gprando55@gmail.com>
2020-06-24 23:48:09 -0300

    erros dealt

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,13 @@
+/* eslint-disable no-console */
 import 'reflect-metadata';
-import express from 'express';
+import express, { Response, Request, NextFunction } from 'express';
+import 'express-async-errors';
+
 import routes from './routes';
 
 import uploadConfig from './config/upload';
+
+import AppError from './errors/AppError';
 
 import './database';
 
@@ -12,7 +17,22 @@ app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
 app.listen(3333, () => {
-  // eslint-disable-next-line no-console
   console.log('🚀 Server running in port 3333!');
 });
