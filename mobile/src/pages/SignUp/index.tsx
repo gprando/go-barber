0cac26c69e25163745120fb370aa73808ed1c407
Gabriel Prando <gprando55@gmail.com>
2020-07-03 09:09:02 -0300

    initial commit usando rn-cli

---
+++ mobile/src/pages/SignUp/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Container } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <View />
+  </Container>
+);
+
+export default SignUp;
