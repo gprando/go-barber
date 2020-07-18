6ec76809a1914107d8fa4893ba797897c9ad3728
Gabriel Prando <gprando55@gmail.com>
2020-07-18 09:18:51 -0300

    testes

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -17,7 +18,24 @@ describe('Create Appointment', () => {
     expect(appointment.provider_id).toBe('1234543');
   });
 
-  // it('should be able to create a new appointment', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentRepository();
+    const createAppointmentService = new CreateAppointmentService(
+      fakeAppointmentRepository,
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointmentService.execute({
+      date: appointmentDate,
+      provider_id: '1234543',
+    });
+
+    expect(
+      createAppointmentService.execute({
+        date: appointmentDate,
+        provider_id: '1234543',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
