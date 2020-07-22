adb417c2bd4335557752199ee737f994ba70fa1e
Gabriel Prando <gprando55@gmail.com>
2020-07-22 08:45:26 -0300

    fix ethereal

--- backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ backend/src/shared/container/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,34 +1,30 @@
+/* eslint-disable no-console */
 import nodemailer, { Transporter } from 'nodemailer';
-
 import IMailProvider from '../models/IMailProvider';
 
 export default class EtherealMailProvider implements IMailProvider {
   private client: Transporter;
 
   constructor() {
-    nodemailer.createTestAccount().then(account => {
-      const transporter = nodemailer.createTransport({
-        host: account.smtp.host,
-        port: account.smtp.port,
-        secure: account.smtp.secure,
-        auth: {
-          user: account.user,
-          pass: account.pass,
-        },
-      });
-
-      this.client = transporter;
+    const transporter = nodemailer.createTransport({
+      host: 'smtp.ethereal.email',
+      port: 587,
+      auth: {
+        user: 'rene20@ethereal.email',
+        pass: '3E85Rvymxtve2NGDA7',
+      },
     });
+
+    this.client = transporter;
   }
 
   public async sendMail(to: string, body: string): Promise<void> {
     const message = await this.client.sendMail({
-      from: 'Equipe GoBarber <equipegobarber.com.b>',
+      from: 'Equipe GoBarber <equipe@gobarber.com.br>',
       to,
-      subject: 'Recuperação de senha',
+      subject: 'Recuperação de Senha',
       text: body,
     });
-
     console.log('Message sent: %s', message.messageId);
 
     console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
